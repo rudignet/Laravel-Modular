f5ab53964196da9c2ca1bac06f9b8de15ee23330
Carlos <rudignet@users.noreply.github.com>
2016-09-08 19:09:43 +0200

    Create ServiceProvider.php

---
+++ src/moduleTemplate/ServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MODULE_REAL_NAMESPACE;
+
+use \Lucid\Modular\ModulesManager;
+use \Route;
+
+class ModulesServiceProvider extends  \Illuminate\Support\ServiceProvider
+{
+    public function boot()
+    {
+        // Boot code, routes, hooks, etc
+
+        //Test route for test purposing
+        Route::group(['middleware' => ['web'], 'prefix' => 'modules/{MODULE_NAME}', 'as'=>'{MODULE_NAME}::', 'namespace' => __NAMESPACE__], function() {
+            Route::get('/',function(){
+               return view('{MODULE_NAME}::welcome',['foo' => config('{MODULE_NAME}.foo')]);
+            });
+        });
+        
+        ModulesManager::attachHook('{MODULE_NAME}.attachPoint1','{MODULE_NAME}::testHook', function(){ return 'TEST-HOOK'; });
+
+    }
+
+
+
+    public function register(){
+        // Register code
+    }
+}
+
