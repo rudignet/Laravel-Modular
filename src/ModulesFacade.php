bcb831bf66fc65719b58a81012ef6cb949faff8e
Carlos <rudignet@users.noreply.github.com>
2016-09-08 23:17:07 +0200

    Create ModulesFacade.php

---
+++ src/ModulesFacade.php
@@ -0,0 +1,16 @@
+<?php namespace Lucid\Modular;
+
+use Illuminate\Support\Facades\Facade;
+
+class ModularFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'modular';
+    }
+}
