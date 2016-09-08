6c27201502bba974fb0119642c134a53ffc031eb
Carlos <rudignet@users.noreply.github.com>
2016-09-08 21:14:11 +0200

    Update ServiceProvider.php

--- src/moduleTemplate/ServiceProvider.php
+++ src/moduleTemplate/ServiceProvider.php
@@ -10,16 +10,13 @@ class ModulesServiceProvider extends  \Illuminate\Support\ServiceProvider
     public function boot()
     {
         // Boot code, routes, hooks, etc
-
         //Test route for test purposing
         Route::group(['middleware' => ['web'], 'prefix' => 'modules/{MODULE_NAME}', 'as'=>'{MODULE_NAME}::', 'namespace' => __NAMESPACE__], function() {
             Route::get('/',function(){
-               return view('{MODULE_NAME}::welcome',['foo' => config('{MODULE_NAME}.foo')]);
+                return view('{MODULE_NAME}::welcome',['foo' => config('{MODULE_NAME}.foo')]);
             });
         });
-        
         ModulesManager::attachHook('{MODULE_NAME}.attachPoint1','{MODULE_NAME}::testHook', function(){ return 'TEST-HOOK'; });
-
     }
 
 
